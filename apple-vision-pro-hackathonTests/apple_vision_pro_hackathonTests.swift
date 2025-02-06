637161744caad19a2df20c267efd21e907fcb3a1
WWIIITT <109016673+WWIIITT@users.noreply.github.com>
2025-02-06 21:54:33 +0800

    Add files via upload

---
+++ apple-vision-pro-hackathonTests/apple_vision_pro_hackathonTests.swift
@@ -0,0 +1,17 @@
+//
+//  apple_vision_pro_hackathonTests.swift
+//  apple-vision-pro-hackathonTests
+//
+//  Created by COMPUser on 6/2/2025.
+//
+
+import Testing
+@testable import apple_vision_pro_hackathon
+
+struct apple_vision_pro_hackathonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
